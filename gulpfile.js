9cb1344c47c505dadc80e16aa781809b505a6a79
Dem Pilafian <dem@centerkey.com>
2018-08-30 22:41:31 -0700

    Less confusing name for html validator

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,27 @@
 // node-slate
 
 // Imports
-const cleanCss =    require('gulp-clean-css');
-const concat =      require('gulp-concat');
-const del =         require('del');
-const ejs =         require('gulp-ejs');
-const fs =          require('fs');
-const gls =         require('gulp-live-server');
-const gulp =        require('gulp');
-const gulpIf =      require('gulp-if');
-const log =         require('fancy-log');
-const highlight =   require('highlight.js');
-const htmlHint =    require('gulp-htmlhint');
-const jsHint =      require('gulp-jshint');
-const marked =      require('marked');
-const mergeStream = require('merge-stream');
-const open =        require('gulp-open');
-const prettify =    require('gulp-prettify');
-const rename =      require('gulp-rename');
-const sass =        require('gulp-sass');
-const uglify =      require('gulp-uglify');
-const w3cJs =       require('gulp-w3cjs');
-const yaml =        require('js-yaml');
+const cleanCss =         require('gulp-clean-css');
+const concat =           require('gulp-concat');
+const del =              require('del');
+const ejs =              require('gulp-ejs');
+const fs =               require('fs');
+const gls =              require('gulp-live-server');
+const gulp =             require('gulp');
+const gulpIf =           require('gulp-if');
+const log =              require('fancy-log');
+const highlight =        require('highlight.js');
+const htmlHint =         require('gulp-htmlhint');
+const jsHint =           require('gulp-jshint');
+const marked =           require('marked');
+const mergeStream =      require('merge-stream');
+const open =             require('gulp-open');
+const prettify =         require('gulp-prettify');
+const rename =           require('gulp-rename');
+const sass =             require('gulp-sass');
+const uglify =           require('gulp-uglify');
+const w3cHtmlValidator = require('gulp-w3cjs');
+const yaml =             require('js-yaml');
 
 // Setup
 const pkg = require('./package.json');
@@ -99,8 +99,8 @@ const task = {
       function ignoreDuplicateIds(type, message) { return !/^Duplicate ID/.test(message); }
       return mergeStream(
          gulp.src('build/index.html')
-            .pipe(w3cJs({ verifyMessage: ignoreDuplicateIds }))
-            .pipe(w3cJs.reporter())
+            .pipe(w3cHtmlValidator({ verifyMessage: ignoreDuplicateIds }))
+            .pipe(w3cHtmlValidator.reporter())
             .pipe(htmlHint(htmlHintConfig))
             .pipe(htmlHint.reporter()),
          gulp.src(jsFiles.scripts)
